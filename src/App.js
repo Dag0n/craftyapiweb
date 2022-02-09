a29f0ed06b66e4130d8eedecad315cc0f5c2a841
Tom Evelin <tom@fxck.co.uk>
2022-02-09 21:18:21 +0000

    pretty

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Main from './Main'
+import Main from "./Main";
 import "./App.css";
 import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
   return (
     <ChakraProvider>
-    <Main />
+      <Main />
     </ChakraProvider>
   );
 }
