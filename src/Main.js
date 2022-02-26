fa7ec7dfaa35b5769dbc25d94665154c25ecdffb
Tom <tom@fxck.co.uk>
2022-02-26 15:26:46 +0000

    fix API

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {
-  Button,
-  Stack,
-  Image,
-  Flex,
-  Input,
-  Box,
-  Divider,
-} from "@chakra-ui/react";
+import { Button, Stack, Flex, Input, Box, Divider } from "@chakra-ui/react";
 import algoliasearch from "algoliasearch";
 import BeerCard from "./components/BeerCard";
 import {
@@ -17,19 +9,45 @@ import {
   connectHits,
 } from "react-instantsearch-dom";
 import axios from "axios";
+import { encode } from "blurhash";
 
 const searchClient = algoliasearch(
   "CXLC6HNNXB",
   "a2f56d56d2d456f27b0814278ac43163"
 );
 
-const index = searchClient.initIndex("beerIndex");
+const index = searchClient.initIndex("craftyBeer");
+
+const loadImage = async (src) =>
+  new Promise((resolve, reject) => {
+    const img = new Image();
+    img.crossOrigin = "anonymous";
+    img.onload = () => resolve(img);
+    img.onerror = (...args) => reject(args);
+    img.src = src;
+  });
+
+const getImageData = (image) => {
+  const canvas = document.createElement("canvas");
+  canvas.width = image.width;
+  canvas.height = image.height;
+  const context = canvas.getContext("2d");
+  context.drawImage(image, 0, 0);
+  return context.getImageData(0, 0, image.width, image.height);
+};
+
+const encodeImageToBlurhash = async (imageUrl) => {
+  const image = await loadImage(imageUrl);
+  const imageData = getImageData(image);
+  return encode(imageData.data, imageData.width, imageData.height, 4, 4);
+};
 
 const Hits = ({ hits, highlightedItem, currentBeerToFind, setToAdd }) => (
-  <Stack spacing={4} direction="row" align="center">
+  <div>
     {hits.map((hit, i) => {
       return (
         <Button
+          style={{ margin: 5 }}
           key={hit.objectID}
           size={highlightedItem.code == `Digit${i + 1}` ? "lg" : "md"}
           colorScheme={highlightedItem.code == `Digit${i + 1}` ? "teal" : "red"}
@@ -43,11 +61,11 @@ const Hits = ({ hits, highlightedItem, currentBeerToFind, setToAdd }) => (
             });
           }}
         >
-          {hit.beer_name}
+          {hit.beer_name}<br />{hit.brewery.brewery_name}
         </Button>
       );
     })}
-  </Stack>
+  </div>
 );
 
 const CustomHits = connectHits(Hits);
@@ -61,53 +79,73 @@ const Main = () => {
 
   useEffect(() => {
     document.addEventListener("keydown", (e) => {
-      if (e.metaKey || e.ctrlKey) {
-        e.preventDefault();
-      }
       setHighlightedItem(e);
-      //   console.log(e)
     });
   });
 
   useEffect(() => {
     if (
-      (highlightedItem.metaKey || highlightedItem.ctrlKey) &&
+      (highlightedItem.metaKey || highlightedItem.altKey) &&
       highlightedItem.code === "KeyS"
     ) {
+      highlightedItem.preventDefault();
       getBeer().then(() => {
         // setHighlightedItem([]);
       });
     }
 
     if (
-      (highlightedItem.metaKey || highlightedItem.ctrlKey) &&
+      (highlightedItem.metaKey || highlightedItem.altKey) &&
       highlightedItem.code === "KeyA"
     ) {
-      addBeer({ ...toAdd, shopId: currentBeerToFind.shopId });
+      highlightedItem.preventDefault();
+      addBeer({
+        ...toAdd,
+        imageArr: currentBeerToFind.images,
+        shopId: currentBeerToFind.shopId,
+      });
     }
 
     if (
-      (highlightedItem.metaKey || highlightedItem.ctrlKey) &&
+      (highlightedItem.metaKey || highlightedItem.altKey) &&
+      highlightedItem.code === "KeyD"
+    ) {
+      highlightedItem.preventDefault();
+      searchBeer(
+        toAdd.vendor + " " + toAdd.title.replace(toAdd.vendor + " ", "")
+      );
+    }
+
+    if (
+      (highlightedItem.metaKey || highlightedItem.altKey) &&
       highlightedItem.code === "KeyE"
     ) {
+      highlightedItem.preventDefault();
       excludeBeer(currentBeerToFind.shopId);
     }
   }, [highlightedItem]);
 
-  //   useEffect(() => {
-  //     getBeer();
-  //   }, []);
+  // useEffect(() => {
+  //   getBeer();
+  // }, []);
 
   const getBeer = async () => {
+    setUntappdBeers([]);
     const result = await axios
-      .get("http://localhost:8080/api/beers/one")
+      .get("https://api.crafty.camp/api/beers/one")
       .then((res) => {
         return res.data;
       })
       .then(({ rows, count }) => {
         let data = rows[0];
-
-        setToAdd({ title: data.title, vendor: data.vendor, brewery: [] });
+        // console.log(data)
+        setToAdd({
+          title: data.title,
+          vendor: data.vendor,
+          shop: data.shop,
+          handle: data.handle,
+          brewery: [],
+        });
         setPendingBeers(count);
         setCurrentBeerToFind(data);
         return { ...count, ...rows };
@@ -117,7 +155,7 @@ const Main = () => {
 
   const excludeBeer = async (id) => {
     const result = await axios
-      .put(`http://localhost:8080/api/beers/exclude/${id}`)
+      .put(`https://api.crafty.camp/api/beers/exclude/${id}`)
       .then((res) => {
         getBeer();
         return res.data;
@@ -125,34 +163,21 @@ const Main = () => {
     return result;
   };
 
-  const indexBeer = async (beerId, brewId) => {
-    const result = await axios
-      .post("http://192.168.1.40/apis/listone.php", {
-        beerId: beerId,
-        brewId: brewId,
-      })
-      .then((res) => {
-        return res.data;
-      })
-      .then((data) => {
-        console.log(data);
-        return data;
-      });
-    return result;
-  };
+  const addImagesToDatabase = (item) => {};
 
   const addBeer = async (beerItem) => {
     const result = await axios
-      .post(`http://localhost:8080/api/beers`, beerItem)
+      .post(`https://api.crafty.camp/api/beers`, beerItem)
       .then((res) => {
-        indexBeer(beerItem.bid, beerItem.brewery.brewery_id);
-
         setToAdd([]);
-        setUntappdBeers([]);
+
         getBeer();
 
         return res.data;
       });
+    beerItem.imageArr.forEach((item) => {
+      addImagesToDatabase(item.src);
+    });
 
     return result;
   };
@@ -183,7 +208,7 @@ const Main = () => {
   return (
     <Flex>
       <Box>
-        {pendingBeers}
+       Remaining: {pendingBeers}
         {currentBeerToFind.length !== 0 && (
           <BeerCard currentBeerToFind={currentBeerToFind} />
         )}
@@ -206,7 +231,11 @@ const Main = () => {
         <Stack spacing={4} direction="row" align="center">
           <Button
             onClick={() =>
-              addBeer({ ...toAdd, shopId: currentBeerToFind.shopId })
+              addBeer({
+                ...toAdd,
+                imageArr: currentBeerToFind.images,
+                shopId: currentBeerToFind.shopId,
+              })
             }
             colorScheme={"teal"}
             size="lg"
@@ -253,9 +282,18 @@ const Main = () => {
             });
           }}
         />
-        <Button>{toAdd.length !== 0 && toAdd.beer_abv}</Button>
+        <Button>{toAdd.length !== 0 && toAdd.beer_abv}%</Button>
+        <Button>£{toAdd.length !== 0 && currentBeerToFind.price}</Button>
+        <Button>
+          <a
+            href={"https://" + toAdd.shop + "/" + toAdd.handle}
+            target="_blank"
+          >
+            Visit shop
+          </a>
+        </Button>
       </Box>
-      <Box flex={1}>
+      <Box flex={2}>
         <InstantSearch searchClient={searchClient} indexName="beerIndex">
           <Configure hitsPerPage={10} analytics={false} />
           <SearchBox
@@ -268,30 +306,31 @@ const Main = () => {
             setToAdd={setToAdd}
           />
         </InstantSearch>
-        {untappdBeers.length !== 0 &&
-          untappdBeers.response.beers.count !== 0 &&
-          untappdBeers.response.beers.items
-            .sort((a, b) => a.beer.beer_name.localeCompare(b.beer.beer_name))
-            .map((item, i) => (
-              <Button
-                key={item.objectID}
-                colorScheme={
-                  highlightedItem.code == `Digit${i + 1}` ? "teal" : "red"
-                }
-                onClick={() => {
-                  // console.log(item);
-                  setToAdd({
-                    ...item.beer,
-                    brewery: item.brewery,
-                    title: item.beer.beer_name,
-                    vendor: item.brewery.brewery_name,
-                    shopBeerId: currentBeerToFind.shopId,
-                  });
-                }}
-              >
-                {item.beer.beer_name}
-              </Button>
-            ))}
+        <div>
+          {untappdBeers.length !== 0 &&
+            untappdBeers.response.beers.count !== 0 &&
+            untappdBeers.response.beers.items
+              .sort((a, b) => a.beer.beer_name.localeCompare(b.beer.beer_name))
+              .map((item, i) => (
+                <Button
+                  style={{ margin: 5 }}
+                  key={item.objectID}
+                  colorScheme={"teal"}
+                  onClick={() => {
+                    // console.log(item);
+                    setToAdd({
+                      ...item.beer,
+                      brewery: item.brewery,
+                      title: item.beer.beer_name,
+                      vendor: item.brewery.brewery_name,
+                      shopBeerId: currentBeerToFind.shopId,
+                    });
+                  }}
+                >
+                  {item.beer.beer_name}<br />{item.brewery.brewery_name}
+                </Button>
+              ))}
+        </div>
       </Box>
     </Flex>
   );
